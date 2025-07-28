2e42239490cb4fa5085812a55401a0a7e0fff810
SamuelMatowski <samuellorenzoepicgames@gmail.com>
2025-07-28 09:45:23 -0300

    oi

---
+++ aula0.js
@@ -0,0 +1,6 @@
+const prompt = require ("prompt-sync")();
+
+let nome = prompt ("Digite seu nome:");
+let idade = prompt ("Digite seu idade:");
+
+console.log ("Bem Vindo",nome, "Você tem",idade ,"anos");
